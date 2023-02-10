f414304f5a082cbcc78f4352cb7321712ed9238f
Leon Forsythe <lvforsythe95@gmail.com>
2023-02-10 16:04:45 -0500

    started work on reducers file for globalstate

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,3 +1,23 @@
 import { useReducer } from "react";
-import { ADD_TODO, REMOVE_TODO, UPDATE_TODO} from "./actions"
+import { ADD_TODO, REMOVE_TODO, UPDATE_TODO } from "./actions";
 
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return {
+        ...state,
+        todos: [...action.todos],
+      };
+
+    case REMOVE_TODO:
+      let newState = state.todos.filter((item) => {
+        return item._id !== action._id;
+      });
+      return {
+        ...state,
+        todos: newState,
+      };
+    default:
+      return state;
+  }
+};
